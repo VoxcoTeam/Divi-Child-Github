ee6a2c6da1d463454b57bc3772a4950ef6b72714
charlesDouc <charles.douc@gmail.com>
2018-10-16 15:41:49 -0400

    Updated the footer with LinkedIn tracking script.

--- footer.php
+++ footer.php
@@ -59,6 +59,22 @@ if ( ! is_page_template( 'page-template-blank.php' ) ) : ?>
 				</div>
 			</footer> <!-- #main-footer -->
 		</div> <!-- #et-main-area -->
+		
+	<!-- LINKEDIN SCRIPTS -->
+	<script type="text/javascript">
+	_linkedin_partner_id = "522650";
+	window._linkedin_data_partner_ids = window._linkedin_data_partner_ids || [];
+	window._linkedin_data_partner_ids.push(_linkedin_partner_id);
+	</script><script type="text/javascript">
+	(function(){var s = document.getElementsByTagName("script")[0];
+	var b = document.createElement("script");
+	b.type = "text/javascript";b.async = true;
+	b.src = "https://snap.licdn.com/li.lms-analytics/insight.min.js";
+	s.parentNode.insertBefore(b, s);})();
+	</script>
+	<noscript>
+	<img height="1" width="1" style="display:none;" alt="" src="https://dc.ads.linkedin.com/collect/?pid=522650&fmt=gif" />
+	</noscript>
 
 <?php endif; // ! is_page_template( 'page-template-blank.php' ) ?>
 
